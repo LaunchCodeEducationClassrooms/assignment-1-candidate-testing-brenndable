4fb5349dee86af7ad48522b794093eea41fadc03
brenprind <brennaprindable@gmail.com>
2021-02-21 02:18:28 +0000

    questions

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,16 +5,24 @@ const input = require('readline-sync');
 
 // TODO 1.1a: Define candidateName  // 
 
-let candidateName= ''
+let candidateName= "";
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 
-let question = ('Who was the first woman in space?');
-let correctAnswer = ('Sally Ride')
-let candidateAnswer = ''
-let questions = ['Who was the first American woman in space? ', 'True or false: 5 kilometer == 5000 meters? ', '(5 + 3)/2 * 10 = ? ', "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 'What is the minimum crew size for the ISS? '];
-let correctAnswers = ['Sally Ride', 'true', '40', 'Trajectory', '3'];
-let candidateAnswers = []
+let question = ('Who was the first woman in space? ');
+let correctAnswer = ('Sally Ride ');
+let candidateAnswer = " ";
+let questions = [ '1: Who was the first American woman in space? ', 
+'2: True or false: 5 kilometer == 5000 meters? ', 
+'3: (5 + 3)/2 * 10 = ? ', 
+'4: Given the array [8, Orbit, Trajectory, 45], what entry is at index 2? ', 
+'5: What is the minimum crew size for the ISS? '];
+let correctAnswers = [ 'Sally Ride', 
+'true', 
+'40', 
+'Trajectory', 
+'3' ];
+let candidateAnswers = [];
 
 
 
