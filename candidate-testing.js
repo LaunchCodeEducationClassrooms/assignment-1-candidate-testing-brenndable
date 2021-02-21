710fb66fc1a8e762cafdc8942aaa3dbe3d864e17
brenprind <brennaprindable@gmail.com>
2021-02-21 00:51:07 +0000

    question

--- candidate-testing.js
+++ candidate-testing.js
@@ -9,8 +9,8 @@ let candidateName= ''
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 
-let question;
-let correctAnswer
+let question = ("Who was the first woman in space");
+let correctAnswer = ("Sally Ride")
 let candidateAnswer = ''
 let questions = ['Who was the first American woman in space? ', 'True or false: 5 kilometer == 5000 meters? ', '(5 + 3)/2 * 10 = ? ', "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 'What is the minimum crew size for the ISS? '];
 let correctAnswers = ['Sally Ride', 'true', '40', 'Trajectory', '3'];
@@ -77,7 +77,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
- console.log(`Name: ${candiateName}`)
+ console.log(`Hi ${candiateName} !`)
 
   askQuestion();
   gradeQuiz(this.candidateAnswers);
