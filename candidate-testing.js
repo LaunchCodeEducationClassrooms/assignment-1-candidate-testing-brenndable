b9ebc71798805ef271c1e79ba9d15b268bfc4196
brenprind <brennaprindable@gmail.com>
2021-02-21 01:04:49 +0000

    1

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,14 +3,14 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 
-// TODO 1.1a: Define candidateName // 
+// TODO 1.1a: Define candidateName  // 
 
 let candidateName= ''
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 
-let question = ("Who was the first woman in space");
-let correctAnswer = ("Sally Ride")
+let question = ('Who was the first woman in space?');
+let correctAnswer = ('Sally Ride')
 let candidateAnswer = ''
 let questions = ['Who was the first American woman in space? ', 'True or false: 5 kilometer == 5000 meters? ', '(5 + 3)/2 * 10 = ? ', "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 'What is the minimum crew size for the ISS? '];
 let correctAnswers = ['Sally Ride', 'true', '40', 'Trajectory', '3'];
