e15123184616b226c132e846628976510a5fb182
brenprind <brennaprindable@gmail.com>
2021-02-21 04:35:45 +0000

    !

--- candidate-testing.js
+++ candidate-testing.js
@@ -88,7 +88,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
- console.log(`Hi ${candidateName} :) \nPlease answer the following questions: \n`);
+ console.log(`Hi ${candidateName}! \nPlease answer the following questions: \n`);
 
 
   askQuestion();
